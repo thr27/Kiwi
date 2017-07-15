d349e25e8c33f1147778dc1d3329c0082bbddbfa
Mr. Senko <atodorov@mrsenko.com>
2017-07-15 12:51:41 +0300

    Mock tcms.core.utils.mailto.mailto function instead of thread.Thread

--- tcms/core/contrib/auth/tests.py
+++ tcms/core/contrib/auth/tests.py
@@ -119,7 +119,8 @@ class TestRegistration(TestCase):
             '<input value="Register" class="loginbutton sprites" type="submit">',
             html=True)
 
-    @patch('tcms.core.utils.mailto.threading.Thread', new=MockThread)
+    # @patch('tcms.core.utils.mailto.threading.Thread', new=MockThread)
+    @patch('tcms.core.utils.mailto.mailto')
     @patch('tcms.core.contrib.auth.models.sha1')
     def assert_user_registration(self, username, sha1):
         print "***** ASSERT USER REGISTRATION called with ", username, sha1
